22c8bf9777627438f3c0c6c3a4f8329744936838
Hillary Hintzen <hillary07@gmail.com>
2016-01-29 11:38:32 -0800

    write third spec and script (do not pass yet)

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
-function Pizza(topping, amount) {
-  this.topping = topping;
+function Pizza(amount, toppings) {
   this.amount = amount;
+  this.toppings = toppings;
 }
-  this.extraToppings = ["pepperoni", "sausage", "olives"];
+  this.toppings = ["pepperoni", "sausage", "olives", "garlic"];
 
 Pizza.prototype.price = 14;
+
+Pizza.prototype.newPrice = function() {
+var numOfToppings = this.toppings.length;
+  var additionalToppings = (numOfToppings - 1)
+  var additionalToppingsPrice = additionalToppings * 2
+  var finalPrice = additionalToppingsPrice + this.price
+return finalPrice;
+};
 
 
 // if (this.topping === "pepperoni") {
--- spec/specs.js
+++ spec/specs.js
@@ -1,13 +1,19 @@
 describe("Pizza", function (){
   it("creates a pizza object to store input properties", function (){
-    var testPizza = new Pizza("cheese","large");
-    expect(testPizza.topping).to.equal("cheese");
+    var testPizza = new Pizza("large","cheese");
+    expect(testPizza.toppings).to.equal("cheese");
     expect(testPizza.amount).to.equal("large");
   });
 
   it("returns the base price of the pizza", function() {
     var testPizza = new Pizza("cheese", "large");
     expect(testPizza.price).to.equal(14);
+  });
+
+  it("will add 2 to the base price for each additional topping selected", function (){
+    var testPizza = new Pizza();
+    testPizza.extraToppings = ["pepperoni", "sausage", "olives", "garlic"];
+    expect(testPizza.newPrice()).to.equal(16);
   });
 });
 
