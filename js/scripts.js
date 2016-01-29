807505f19cb01243501726ef0c5788d05519b18c
Hillary Hintzen <hillary07@gmail.com>
2016-01-29 13:38:50 -0800

    rewrote first and second spec for clarity

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,22 @@
-function Pizza(amount, topping, extraToppings) {
+function Pizza(amount, toppings) {
   this.amount = amount;
-  this.topping = topping;
-  this.extraToppings = extraToppings;
+  this.toppings = toppings;
 }
 
 Pizza.prototype.price = 14;
 
-Pizza.prototype.newPrice = function() {
-var numOfToppings = this.toppings.length;
-  var additionalToppings = (numOfToppings - 1)
-  var additionalToppingsPrice = additionalToppings * 2
-  var finalPrice = additionalToppingsPrice + this.price
-return finalPrice;
-};
-
+// Pizza.prototype.newPrice = function() {
+//  var toppingsPrice = 0
+//  var sizePricee = 0
+//  if (this.amount === "regular") {
+//      sizePrice = 14;
+//    } else if (this.amount === "large") {
+//      sizePrice = 18;
+//      else if (this.amount === "small")
+//        toppingsPrice = this.toppingNum * 1;
+//      } else if (this.amount === "large") {
+//        toppingsPrice = this.toppingNum * 1.5;
+//      }
 
 // if (this.topping === "pepperoni") {
 //   pizzaPrice += 1;
--- spec/specs.js
+++ spec/specs.js
@@ -1,21 +1,26 @@
-describe("Pizza", function (){
-  it("creates a pizza object to store input properties as indicated", function (){
-    var testPizza = new Pizza("large","cheese",["pepperoni", "sausage", "olives", "garlic"]);
-    expect(testPizza.topping).to.equal("cheese");
-    expect(testPizza.amount).to.equal("large");
-    expect(testPizza.extraToppings).to.eql(["pepperoni", "sausage", "olives", "garlic"]);
-  });
+describe("Pizza", function() {
+  it("creates a new Pizza object with properties for name, size. topping numbers, and crust type", function() {
+      var testPizza = new Pizza("regular", ["pepperoni", "sausage", "olives", "garlic"]);
+      expect(testPizza.amount).to.equal("regular");
+      expect(testPizza.toppings).to.eql(["pepperoni", "sausage", "olives", "garlic"]);
+    });
 
-  it("returns the base price of the pizza", function() {
-    var testPizza = new Pizza("cheese", "large",[]);
+  it("adds a method to calculate pizza price", function() {
+    var testPizza = new Pizza("cheese", "regular",[["pepperoni", "sausage", "olives", "garlic"]]);
     expect(testPizza.price).to.equal(14);
   });
-
-  it("will add $2 to the base price for each additional topping selected", function (){
-    var testPizza = new Pizza("cheese", "large",["pepperoni"]);
-    expect(testPizza.price()).to.equal(16);
-  });
 });
+
+  // it("charges $14 for a regular pizza,", function(){
+  //   var testPizza = new Pizza("cheese", "regular",[]);
+  //   expect(testPizza.price()).to.equal(14);
+  // });
+  //
+  // it("will add $2 to the base price for each additional topping selected", function (){
+  //   var testPizza = new Pizza("cheese", "regular",["pepperoni"]);
+  //   expect(testPizza.newPrice()).to.equal(16);
+  // });
+// });
 
 
 
