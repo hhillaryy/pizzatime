5acc42ef7c13c3b85a997f370f0505d29e18c4b6
Hillary Hintzen <hillary07@gmail.com>
2016-01-29 11:10:10 -0800

    write and pass second spec, create HTML layout

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,11 @@ function Pizza(topping, amount) {
   this.topping = topping;
   this.amount = amount;
 }
+  this.extraToppings = ["pepperoni", "sausage", "olives"];
+
+Pizza.prototype.price = 14;
+
+
+// if (this.topping === "pepperoni") {
+//   pizzaPrice += 1;
+// }
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,16 @@
 describe("Pizza", function (){
   it("creates a pizza object to store input properties", function (){
-    var testPizza = new Pizza("pepperoni","large");
-    expect(testPizza.topping).to.equal("pepperoni");
+    var testPizza = new Pizza("cheese","large");
+    expect(testPizza.topping).to.equal("cheese");
     expect(testPizza.amount).to.equal("large");
+  });
+
+  it("returns the base price of the pizza", function() {
+    var testPizza = new Pizza("cheese", "large");
+    expect(testPizza.price).to.equal(14);
+  });
 });
 
-});
+
+
+//   var testPizza = new Pizza ()
