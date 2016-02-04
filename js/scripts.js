765a60682d9da16c5fc58ee7c8920114a603c81a
Hillary Hintzen <hillary07@gmail.com>
2016-02-03 17:36:53 -0800

    update jQuery to make site run edit styles for nicer result display

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,14 @@ Pizza.prototype.price = function() {
   var pizzaPrice = 0;
   var sizePrice = 0;
   var toppingPrice = 0;
-  if (this.amount === "regular") {
+  if (this.amount === "Regular") {
     sizePrice = 14;
-  } else if (this.amount === "large") {
+  } else if (this.amount === "Large") {
     sizePrice = 16;
   } else {}
-  if (this.amount === "regular") {
+  if (this.amount === "Regular") {
     toppingPrice = this.toppingnum * 2;
-  } else if (this.amount === "large") {
+  } else if (this.amount === "Large") {
     toppingPrice = this.toppingnum * 2.5;
   }
   pizzaPrice = sizePrice + toppingPrice;
@@ -32,19 +32,19 @@ var getToppings = function() {
 }
 
 // need to update the doc ready jquery section that calls back to user inputs to the pizza constructor and generates and displays the result div. Still troubleshooting checkbox inputs too.
-$(document).ready(function () {
+$(document).ready(function() {
   $("form#pizza-calculator").submit(function(event) {
     event.preventDefault();
     // gets input from page, including running the functions for the toppings
-    var size = $('input[name="size"]:checked').val();
+    var size = $("input[name='size']:checked").val();
+  // debugger;
     var toppings = getToppings();
 
-  });
 
-  // var newPizza = new Pizza(amount, toppings);
-  //   var total = newPizza.price
-  //   "pizza-size".text(newPizza.amount);
-  //   "pizza-price".text(newPizza.price());
-  //   "toppings-list".text(newPizza.toppings)
-  //   "order-total".text(total);
+    var newPizza = new Pizza(size, toppings);
+      var total = newPizza.price
+      $(".pizza-size").text(newPizza.amount);
+      $(".pizza-price").text(newPizza.price());
+      $(".toppings-list").text(newPizza.toppings);
+  });
 });
